1094d0fb8239f102aa55c382e768b6d3dab6c72d
kendsr <kndulin@comcast.net>
2016-06-27 09:57:33 -0500

    initial commit

---
+++ routes/index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+module.exports = function(app) {
+	// Landing Page
+    app.get('/', function(req, res) {
+    	var title = "Whoami Microservice";
+        res.render('index', {title: title});
+    });
+
+    app.get('/api/whoami', function(req, res) {
+        var headers = req.headers;
+        console.log(headers);
+        var obj = {"ipaddress":headers['host'],
+        	   "Language":headers['accept-language'],
+        	   "Software":headers['user-agent']
+        	   };
+   		res.json(obj);
+    });
+};
